e29f86a8cf5454af9d3ec5ca102242cfaa4cf791
Veljko Naumovic <veljko.naumovic@gmail.com>
2026-05-23 21:24:51 +0200

    Add analytics chart, change grid with flex in statsCard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,11 +1,15 @@
 import DashboardStats from "@/features/dashboard/components/DashboardStats/DashboardStats";
 
+import AnalyticsChart from "@/features/dashboard/components/AnalyticsChart/AnalyticsChart";
+
 export default function DashboardPage() {
 	return (
 		<div>
 			<h1>Dashboard Overview</h1>
 
 			<DashboardStats />
+
+			<AnalyticsChart />
 		</div>
 	);
 }
---
+++ src/features/mock/analytics.data.ts
@@ -0,0 +1,30 @@
+export const analyticsData = [
+	{
+		day: "Mon",
+		requests: 1200,
+	},
+	{
+		day: "Tue",
+		requests: 1900,
+	},
+	{
+		day: "Wed",
+		requests: 800,
+	},
+	{
+		day: "Thu",
+		requests: 2400,
+	},
+	{
+		day: "Fri",
+		requests: 1800,
+	},
+	{
+		day: "Sat",
+		requests: 900,
+	},
+	{
+		day: "Sun",
+		requests: 1500,
+	},
+];
