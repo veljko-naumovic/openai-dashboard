84dfdee83902b4630243c909cec43a5e1a768b2e
Veljko Naumovic <veljko.naumovic@gmail.com>
2026-05-24 11:07:03 +0200

    Add active link in sidebar

--- src/components/layout/Sidebar/Sidebar.tsx
+++ src/components/layout/Sidebar/Sidebar.tsx
@@ -1,23 +1,36 @@
+"use client";
+
 import Link from "next/link";
+
+import { usePathname } from "next/navigation";
+
 import styles from "./Sidebar.module.scss";
 
 import { SIDEBAR_ITEMS } from "@/constants/sidebar";
 
 export default function Sidebar() {
+	const pathname = usePathname();
+
 	return (
 		<aside className={styles.sidebar}>
 			<div className={styles.logo}>OpenAI Dashboard</div>
 
 			<nav className={styles.navigation}>
-				{SIDEBAR_ITEMS.map((item) => (
-					<Link
-						key={item.href}
-						href={item.href}
-						className={styles.link}
-					>
-						{item.label}
-					</Link>
-				))}
+				{SIDEBAR_ITEMS.map((item) => {
+					const isActive = pathname === item.href;
+
+					return (
+						<Link
+							key={item.href}
+							href={item.href}
+							className={`${styles.link} ${
+								isActive ? styles.active : ""
+							}`}
+						>
+							{item.label}
+						</Link>
+					);
+				})}
 			</nav>
 		</aside>
 	);
